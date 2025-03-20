0c994bffef8433bcfc867a378dac49c453064257
Memet Akboğa <makboga@k1m30s07.42istanbul.com.tr>
2025-03-20 22:09:13 +0300

    check

---
+++ so_Long/printf/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: makboga <makboga@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 18:01:40 by makboga           #+#    #+#             */
+/*   Updated: 2024/11/26 18:01:40 by makboga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int n);
+int	ft_puthex(unsigned long long n, int uppercase);
+int	ft_putptr(unsigned long long ptr);
+
+#endif
